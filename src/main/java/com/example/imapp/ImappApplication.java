7230c9e5bd596cd03ec1264638d2e5168785b146
akiniwa-dev <dakiniwa@gmail.com>
2024-11-30 09:46:24 +0900

    initial commit

---
+++ src/main/java/com/example/imapp/ImappApplication.java
@@ -0,0 +1,13 @@
+package com.example.imapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imapp/ImappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
