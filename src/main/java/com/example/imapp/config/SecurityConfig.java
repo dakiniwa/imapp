1e69557902621b856b4ed13bf5638b379aed13be
akiniwa-dev <dakiniwa@gmail.com>
2024-11-30 10:55:09 +0900

    認証機能実装
     - ログイン
     - ログアウト

--- src/main/java/com/example/imapp/config/SecurityConfig.java
+++ src/main/java/com/example/imapp/config/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.savedrequest.NullRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
 
 @Configuration
 @EnableWebSecurity
@@ -12,11 +14,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        RequestCache nullRequestCache = new NullRequestCache();
+
         http.authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login/**").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(login -> login
-                        .loginPage("/login"));
+                        .loginPage("/login"))
+                .requestCache(cache -> cache
+                        .requestCache(nullRequestCache));
 
         return http.build();
     }
