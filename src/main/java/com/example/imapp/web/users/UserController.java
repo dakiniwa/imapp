c0c1c8cfa4e0b02cf55c95504b9e587799ce7e2a
akiniwa-dev <dakiniwa@gmail.com>
2024-12-03 22:37:11 +0900

    ユーザ情報管理
     - ユーザ一覧
     - ユーザ作成

---
+++ src/main/java/com/example/imapp/web/users/UserController.java
@@ -0,0 +1,36 @@
+package com.example.imapp.web.users;
+
+import com.example.imapp.domain.auth.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String showList(Model model) {
+        model.addAttribute("userList", userService.findAll());
+        return "users/list";
+    }
+
+    @GetMapping("/creationForm")
+    public String showCreationForm(@ModelAttribute UserForm form) {
+        return "users/creationForm";
+    }
+
+    @PostMapping
+    public String create(UserForm form) {
+        return "redirect:/users";
+    }
+}
---
+++ src/main/java/com/example/imapp/web/users/UserForm.java
@@ -0,0 +1,12 @@
+package com.example.imapp.web.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserForm {
+
+    private String username;
+    private String password;
+}
